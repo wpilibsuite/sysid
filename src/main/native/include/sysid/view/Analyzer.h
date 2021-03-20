52e0146588b1f92e1c014cc65c9ebf0c07eae6fe
Prateek Machiraju <prateek.machiraju@gmail.com>
2021-03-19 22:32:38 -0400

    Refactor plotting into separate class (#71)

--- src/main/native/include/sysid/view/Analyzer.h
+++ src/main/native/include/sysid/view/Analyzer.h
@@ -22,6 +22,7 @@
 #include "sysid/analysis/AnalysisType.h"
 #include "sysid/analysis/FeedbackAnalysis.h"
 #include "sysid/analysis/FeedbackControllerPreset.h"
+#include "sysid/view/AnalyzerPlot.h"
 
 struct ImPlotPoint;
 
@@ -45,58 +46,6 @@ class Analyzer : public glass::View {
   void PrepareData();
   void Calculate();
   void ResetManagerState();
-
-  struct PlotData {
-    const char* name;
-    ImPlotPoint (*getter)(void*, int);
-    std::function<std::vector<PreparedData>*()> data;
-
-    const char* xlabel;
-    const char* ylabel;
-  };
-
-  /**
-   * Plots time domain simulation of feedforward gains
-   */
-  template <typename Model>
-  void PlotTimeDomainSim(const PlotData& data, Model model) {
-    ImPlot::SetNextLineStyle(IMPLOT_AUTO_COL, 1.5);
-
-    std::vector<double> xs;
-    std::vector<double> ys;
-
-    // Get data list and initialize model state
-    auto preparedData = *(data.data());
-    xs.emplace_back(0.0);
-    ys.emplace_back(preparedData[0].velocity);
-
-    model.Reset(preparedData[0].position, preparedData[0].velocity);
-    units::second_t t = 0_s;
-    for (int j = 1; j < preparedData.size(); ++j) {
-      const auto& prev = preparedData[j - 1];
-      const auto& now = preparedData[j];
-
-      auto dt =
-          units::second_t{now.timestamp} - units::second_t{prev.timestamp};
-      t += dt;
-
-      // If there's a large gap or the time went backwards, it's a new
-      // section of data, so reset the model state
-      if (dt < 0_s || dt > 1_s) {
-        ImPlot::PlotLine("##Fit", &xs[0], &ys[0], xs.size());
-        xs.clear();
-        ys.clear();
-        model.Reset(now.position, now.velocity);
-        continue;
-      }
-
-      model.Update(units::volt_t{prev.voltage}, dt);
-
-      xs.emplace_back(t);
-      ys.emplace_back(model.GetVelocity());
-    }
-    ImPlot::PlotLine("##Fit", &xs[0], &ys[0], xs.size());
-  }
 
   bool first = true;
   std::string m_exception;
@@ -128,15 +77,14 @@ class Analyzer : public glass::View {
   int m_window = 8;
   double m_threshold = 0.2;
 
-  // Plotting
-  std::vector<PlotData> m_timeDomainData;
-  std::vector<PlotData> m_voltageDomainData;
-
   // File manipulation
   std::unique_ptr<pfd::open_file> m_selector;
   std::string* m_location;
 
   // Logger
   wpi::Logger& m_logger;
+
+  // Plot
+  AnalyzerPlot m_plot{m_logger};
 };
 }  // namespace sysid
