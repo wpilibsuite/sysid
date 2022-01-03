2205c577870c5f08d1d3a806f4289f99fdf27968
Matteo Kimura <mateus.sakata@gmail.com>
2022-01-03 13:22:04 -0600

    Add PWF Venom Support for 2022 (#290)

--- sysid-library/src/main/cpp/generation/SysIdSetup.cpp
+++ sysid-library/src/main/cpp/generation/SysIdSetup.cpp
@@ -6,7 +6,7 @@
 
 #include <stdexcept>
 
-// #include <CANVenom.h>
+#include <CANVenom.h>
 #include <fmt/core.h>
 #include <frc/ADXRS450_Gyro.h>
 #include <frc/AnalogGyro.h>
@@ -93,16 +93,12 @@ void AddMotorController(
     sparkMax->SetIdleMode(rev::CANSparkMax::IdleMode::kBrake);
   } else if (controller == "Venom") {
     fmt::print("Setup Venom");
-#ifdef __FRC_ROBORIO__
-    //   controllers->emplace_back(std::make_unique<frc::CANVenom>(port));
+    controllers->emplace_back(std::make_unique<frc::CANVenom>(port));
 
-    //   auto* venom = static_cast<frc::CANVenom*>(controllers->back().get());
+    auto* venom = static_cast<frc::CANVenom*>(controllers->back().get());
 
-    //   venom->SetInverted(inverted);
-    //   venom->SetBrakeCoastMode(frc::CANVenom::BrakeCoastMode::kBrake);
-#else
-    controllers->emplace_back(std::make_unique<frc::Spark>(port));
-#endif
+    venom->SetInverted(inverted);
+    venom->SetBrakeCoastMode(frc::CANVenom::BrakeCoastMode::kBrake);
   } else {
     fmt::print("Setup PWM");
     controllers->emplace_back(std::make_unique<frc::Spark>(port));
@@ -186,12 +182,12 @@ void SetupEncoders(
                        encoderInverted, position, rate);
     } else {  // Venom
       fmt::print("Setup Built-in+Venom");
-      //   auto* venom = static_cast<frc::CANVenom*>(controller);
-      //   position = [=] { return venom->GetPosition(); };
-      //   rate = [=] {
-      //     return venom->GetSpeed() /
-      //            60;  // Conversion from RPM to rotations per second
-      //   };
+      auto* venom = static_cast<frc::CANVenom*>(controller);
+      position = [=] { return venom->GetPosition(); };
+      rate = [=] {
+        return venom->GetSpeed() /
+               60;  // Conversion from RPM to rotations per second
+      };
     }
   } else if (encoderType == "Encoder Port") {
     auto* sparkMax = static_cast<rev::CANSparkMax*>(controller);
