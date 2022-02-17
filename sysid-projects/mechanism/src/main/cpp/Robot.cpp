ef031718f804e269cc01dff348cdf37e78520981
Tyler Veness <calcmogul@gmail.com>
2022-02-17 13:28:31 -0800

    Don't push NetworkTables entries in autonomous (#372)

--- sysid-projects/mechanism/src/main/cpp/Robot.cpp
+++ sysid-projects/mechanism/src/main/cpp/Robot.cpp
@@ -69,24 +69,7 @@ Robot::Robot() : frc::TimedRobot(5_ms) {
 
 void Robot::RobotInit() {}
 
-/**
- * This function is called every robot packet, no matter the mode. Use
- * this for items like diagnostics that you want ran during disabled,
- * autonomous, teleoperated and test.
- *
- * <p> This runs after the mode specific periodic functions, but before
- * LiveWindow and SmartDashboard integrated updating.
- */
-void Robot::RobotPeriodic() {
-  try {
-    frc::SmartDashboard::PutNumber("Position", m_position());
-    frc::SmartDashboard::PutNumber("Rate", m_rate());
-  } catch (std::exception& e) {
-    fmt::print("Project failed: {}\n", e.what());
-    std::exit(-1);
-  }
-  // TODO Put actual readings once supported
-}
+void Robot::RobotPeriodic() {}
 
 /**
  * This autonomous (along with the chooser code above) shows how to select
@@ -113,7 +96,9 @@ void Robot::AutonomousPeriodic() {
 
 void Robot::TeleopInit() {}
 
-void Robot::TeleopPeriodic() {}
+void Robot::TeleopPeriodic() {
+  PushNTDiagnostics();
+}
 
 void Robot::DisabledInit() {
   sysid::SetMotorControllers(0_V, m_controllers);
@@ -136,11 +121,25 @@ void Robot::SimulationPeriodic() {
 #endif
 }
 
-void Robot::DisabledPeriodic() {}
+void Robot::DisabledPeriodic() {
+  PushNTDiagnostics();
+}
 
 void Robot::TestInit() {}
 
-void Robot::TestPeriodic() {}
+void Robot::TestPeriodic() {
+  PushNTDiagnostics();
+}
+
+void Robot::PushNTDiagnostics() {
+  try {
+    frc::SmartDashboard::PutNumber("Position", m_position());
+    frc::SmartDashboard::PutNumber("Rate", m_rate());
+  } catch (std::exception& e) {
+    fmt::print("Project failed: {}\n", e.what());
+    std::exit(-1);
+  }
+}
 
 #ifndef RUNNING_FRC_TESTS
 int main() {
