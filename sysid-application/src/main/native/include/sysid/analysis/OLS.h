921fff1efb904b46453571533998d767ea690ff3
Tyler Veness <calcmogul@gmail.com>
2023-09-23 23:11:10 -0700

    Refactor OLS

--- sysid-application/src/main/native/include/sysid/analysis/OLS.h
+++ sysid-application/src/main/native/include/sysid/analysis/OLS.h
@@ -8,18 +8,19 @@
 #include <tuple>
 #include <vector>
 
+#include <Eigen/Core>
+
 namespace sysid {
+
 /**
  * Performs ordinary least squares multiple regression on the provided data and
  * returns a vector of coefficients along with the r-squared (coefficient of
  * determination) and RMSE (stardard deviation of the residuals) of the fit.
  *
- * @param XData The independent data in y = Xβ. The data must be formatted as
- *   x₀, x₁, x₂, ..., in the vector.
- * @param independentVariables The number of independent variables (x values).
- * @param yData The dependent data in y = Xβ.
+ * @param X The independent data in y = Xβ.
+ * @param y The dependent data in y = Xβ.
  */
-std::tuple<std::vector<double>, double, double> OLS(
-    const std::vector<double>& XData, size_t independentVariables,
-    const std::vector<double>& yData);
+std::tuple<std::vector<double>, double, double> OLS(const Eigen::MatrixXd& X,
+                                                    const Eigen::VectorXd& y);
+
 }  // namespace sysid
